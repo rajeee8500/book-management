29fefb8d348eca6ede4ab709c69cbe1b000b3c83
Rajee <rajee.katta94@gmail.com>
2024-03-17 21:16:17 +0530

    first comt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [bookName, setBookName] = useState("");
+  const [price, setprice] = useState("");
+  const [author, setauthor] = useState("");
+  const [publisher, setpublisher] = useState("");
+  const [address, setaddress] = useState("");
+
+  const getUser = async () => {
+    let response = await fetch("http://localhost:8080/bookfrom", {
+      method: "GET",
+      headers: { "content-type": "application/json" },
+    });
+    let json = await response.json();
+    console.log(json);
+  };
+  const btnClicked = async () => {
+    let data = {
+      bookname: bookName,
+      price: price,
+      author: author,
+      publisher: publisher,
+      address: address,
+    };
+
+    let response = await fetch("http://localhost:8080/booklist", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "content-type": "application/json" },
+    });
+    console.log(response);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={bookName}
+        onChange={(e) => setBookName(e.target.value)}
+        placeholder="enter book name"
+      />
+      <input
+        type="number"
+        value={price}
+        onChange={(e) => setprice(e.target.value)}
+        placeholder="enter price"
+      />
+      <input
+        type="text"
+        value={author}
+        onChange={(e) => setauthor(e.target.value)}
+        placeholder="enter author name"
+      />
+      <input
+        type="text"
+        value={publisher}
+        onChange={(e) => setpublisher(e.target.value)}
+        placeholder="enter publisher"
+      />
+      <input
+        type="text"
+        value={address}
+        onChange={(e) => setaddress(e.target.value)}
+        placeholder="enter address"
+      />
+      <button onClick={getUser}>click to get users</button>;
+      <button onClick={btnClicked}>click to post users</button>;
     </div>
   );
 }
